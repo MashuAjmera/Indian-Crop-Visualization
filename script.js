30a6b11277f8aa3b16fa62bd633bf153bb9b9d12
Mashu Ajmera <mashuajmera@gmail.com>
2020-12-20 22:45:30 +0530

    select option functionality enaled

--- script.js
+++ script.js
@@ -1,54 +1,110 @@
-let year = document.getElementById("year").value;
-let season = document.getElementById("season").value;
-let crop = document.getElementById("crop").value;
-
 let drawSpace = d3.select("#drawspace");
-Promise.all([d3.csv("tempdata.csv"), d3.json("states.json")]).then(
-  showData
-);
+Promise.all([d3.csv("crop.csv"), d3.json("states.json")]).then(showData);
+
+var mapInfo, reqData, drawSpaceW,drawSpaceH,cropInfo;
 
 function showData(datasources) {
-  let drawSpaceH = 600;
-  let drawSpaceW = 600;
+  drawSpaceH = 600;
+  drawSpaceW = 600;
 
-  let populationInfo = datasources[0];
-  let mapInfo = datasources[1];
+  cropInfo = datasources[0];
+  mapInfo = datasources[1];
 
-  let dataIndex = {};
-  for (let c of populationInfo) {
-    let country = c.States;
-    dataIndex[country] = c.NPN;
+  let sel='';
+
+  let crop = [...new Set(cropInfo.map((d) => d.Crop))].sort();
+  crop.forEach(y=>sel+=`<option value="${y}">${y}</option>`)
+  document.getElementById("crop").innerHTML= sel;
+  
+  let year = [...new Set(cropInfo.map((d) => d.Crop_Year))].sort();
+  sel='';
+  year.forEach(y=>sel+=`<option value="${y}">${y}</option>`)
+  document.getElementById("year").innerHTML= sel;
+
+  let season = [...new Set(cropInfo.map((d) => d.Season))].sort();
+  sel='';
+  season.forEach(y=>sel+=`<option value="${y}">${y}</option>`)
+  document.getElementById("season").innerHTML= sel;
+
+  // console.log(mapInfo);
+
+  change();
+}
+
+function change(){
+  let states = [...new Set(cropInfo.map((d) => d.State_Name))];
+  
+  let yearReq = document.getElementById("year").value;
+  let cropReq = document.getElementById("crop").value;
+  // cropReq = "Rice";
+  console.log(`year: ${yearReq}, crop: ${cropReq}`);
+
+  let cropInfoDist = {};
+  cropInfoDist = cropInfo
+    .map((d) => {
+      if (yearReq == d.Crop_Year && cropReq == d.Crop) return d;
+    })
+    .filter((d) => d);
+  // console.log(cropInfoDist);
+
+  let prodData = {};
+  for (let c of cropInfoDist) {
+    let state = c.State_Name;
+    if (parseFloat(c.Production) !== NaN || c.Production !== "") {
+      if (state in prodData) prodData[state] += parseFloat(c.Production);
+      else prodData[state] = parseFloat(c.Production);
+    }
   }
+  // console.log(prodData)
+
+  let areaData = {};
+  for (let c of cropInfoDist) {
+    let state = c.State_Name;
+    if (parseFloat(c.Area) !== NaN || c.Area !== "") {
+      if (state in areaData) areaData[state] += parseFloat(c.Area);
+      else areaData[state] = parseFloat(c.Area);
+    }
+  }
+  // console.log(areaData)
+
+  reqData = {};
+  for (let state of states) {
+    if (state in prodData && state in areaData) {
+      reqData[state] = prodData[state] / areaData[state];
+    } else {
+      reqData[state] = 0;
+    }
+  }
+  // console.log(reqData)
 
   mapInfo.features = mapInfo.features.map((d) => {
-    let country = d.properties.st_nm;
-    let population = dataIndex[country];
-    d.properties.population = population;
+    let state = d.properties.st_nm;
+    let prodPerArea = reqData[state];
+    d.properties.prodPerArea = prodPerArea;
     return d;
   });
+  // console.log(mapInfo.features)
 
-  let maxPopulation = d3.max(
+  let maxProdPerArea = d3.max(
     mapInfo.features,
-    (d) => d.properties.population
+    (d) => d.properties.prodPerArea
   );
-  let medianPopulation = d3.median(
-    mapInfo.features,
-    (d) => d.properties.population
-  );
+
   let cScale = d3
     .scaleLinear()
-    .domain([0, maxPopulation])
-    .range(["white", "red"]);
-    
-  let color = d3.scaleQuantize([0, maxPopulation], d3.schemeReds[6]);
-  drawSpace.append("g")
-        .attr("transform", "translate("+drawSpaceW/2+",0)")
-        .append(() => legend({color, title: "Efficiency", width: 260}));
+    .domain([0, maxProdPerArea])
+    .range(["white", "brown"]);
 
-  let myProjection = d3
-    .geoMercator()
-    .scale(950)
-    .translate([-1050, 680]);
+  let color = d3.scaleQuantize([0, maxProdPerArea], d3.schemeReds[6]);
+drawSpace.select("g").remove();
+drawSpace.selectAll("path").remove();
+
+  drawSpace
+    .append("g")
+    .attr("transform", "translate(" + drawSpaceW / 2 + ",0)")
+    .append(() => legend({ color, title: "Efficiency", width: 260 }));
+
+  let myProjection = d3.geoMercator().scale(950).translate([-1050, 680]);
   let geoPath = d3.geoPath().projection(myProjection);
 
   drawSpace
@@ -58,20 +114,15 @@ function showData(datasources) {
     .append("path")
     .attr("d", (d) => geoPath(d))
     .attr("stroke", "black")
-    // .attr("fill", (d) => cScale(d.properties.population)); // black for missing by default
     .attr("fill", (d) =>
-      d.properties.population ? cScale(d.properties.population) : "blue"
-    ).on("mouseenter", function () {
+      d.properties.prodPerArea ? cScale(d.properties.prodPerArea) : "white"
+    )
+    .on("mouseenter", function () {
       this.style.opacity = 0.8;
-      //   d3.select(this).attr("fill", "blue"); // try this
-    }).on("mouseout", function () {
+    })
+    .on("mouseout", function () {
       this.style.opacity = 1;
-      //   d3.select(this).attr("fill", "blue"); // try this
     })
     .append("title")
-      .text(d => `${d.properties.st_nm}: ${d.properties.population} `);
-    
-  //   .attr("fill", (d) =>
-  //     d.properties.population ? cMScale(d.properties.population) : "blue"
-  //   ); // for divergent scale
+    .text((d) => `${d.properties.st_nm}: ${d.properties.prodPerArea}`);
 }
